7181afd4d45c9cba8da8a6177b3d5f04bb085168
Chandan Kumar <chandankumar99341@gmail.com>
2021-04-15 11:58:13 +0530

    Find the Smallest Common Number

---
+++ Data-Structure-Problems/Arrays/Find-the-Smallest-Common-Number.js
@@ -0,0 +1,32 @@
+// Runtime Complexity: Linear O(n) : Traverses the array only once
+// Memory Complexity: Constant O(1)
+
+let findLeastCommonNumber = function(a, b, c) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (i < a.length && j < b.length && k < c.length) {
+    // Finding the smallest common number
+    if (a[i] === b[j] && b[j] === c[k]) {
+      return a[i];
+    }
+
+    // Find the smallest and increment it
+    // a[i] is smallest
+    if (a[i] < b[j] && a[i] < c[k]) {
+      i++;
+    } else if (b[j] < a[i] && b[j] < c[k]) {
+      j++;
+    // } else if (c[k] < a[i] && c[k] < b[j]) { // can cause infinite loop if none increases (in case two values are equal but other is not)
+    //   k++;
+    // }
+    } else {
+      k++;
+    }
+  }
+  return -1;
+};
+
+// console.log(findLeastCommonNumber([1, 4, 6, 7, 8, 10, 14], [1, 4, 5, 6, 7, 8, 50], [0, 6, 7, 8, 10, 25, 30, 40]))
+console.log('Least common number: ', findLeastCommonNumber([6,7,10,25,30,63,64], [1,4,5,6,7,8,50], [1,6,10,14]))
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ by [Chandan Kumar](https://enchandan.com/)
 | --- | -------- | -------- | --------- | --------- |
 | 1. | `Array` | [LeetCode](https://leetcode.com/problems/sliding-window-maximum/) | [Find Maximum in Sliding Window](Data-Structure-Problems/Arrays/Find-Maximum-in-Sliding-Window.js) | [Find Maximum in Sliding Window Optimized](Data-Structure-Problems/Arrays/Find-Maximum-in-Sliding-Window-Optimized.js) |
 | 2. | `Array` | [LeetCode](https://leetcode.com/problems/search-in-rotated-sorted-array/) | [Search a Rotated Array Iterative](Data-Structure-Problems/Arrays/Search-a-Rotated-Array-Iterative.js) | [Search a Rotated Array Recursive](Data-Structure-Problems/Arrays/Search-a-Rotated-Array-Recursive.js) |
+| 3. | `Array` |  | [Find the Smallest Common Number](Data-Structure-Problems/Arrays/Find-the-Smallest-Common-Number.js) |  |
