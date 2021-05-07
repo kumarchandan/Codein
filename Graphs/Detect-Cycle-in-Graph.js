d4dc7b961dbb3036a362f51dea0d42f5a749d026
Chandan Kumar <chandankumar99341@gmail.com>
2021-05-07 22:57:22 +0530

    detect cycle in a graph - 1

---
+++ Graphs/Detect-Cycle-in-Graph.js
@@ -0,0 +1,17 @@
+"use strict";
+const LinkedList = require('./LinkedList.js');
+const Node = require('./Node.js');
+const Stack = require('./Stack.js');
+const Graph = require('./Graph.js');
+
+/**
+* Create Stack => let stack = new Stack(5), where 5 is the size of the stack
+* Functions of Stack => stack.push(int), stack.pop(), getTop(), isEmpty() 
+* class Graph => {int vertices, linkedList[] list}
+* class linkedList => {Node head}
+* class Node => {int data, Node nextElement}
+*/
+function detectCycle(graph){
+    
+    return null; 
+}
