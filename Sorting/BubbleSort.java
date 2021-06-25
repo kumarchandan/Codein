ada86a7d4a2d22752bac5dd81a2b32792fe85407
Chandan Kumar <chandankumar99341@gmail.com>
2021-06-25 20:11:24 +0530

    Add Sorting Algorithms

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,38 @@
+/**
+ * Runtime:
+ * Worst case O(n^2)
+ * Average case O(n^2)
+ * Best case O(n)
+ */
+public class BubbleSort {
+    static void bubbleSort(int arr[], int arrSize) {
+        int i, j, temp;
+        // Traverse through all array elements
+        for (i = 0; i < arrSize - 1; i++) {
+            // Last i elements are already in place      
+            for (j = 0; j < arrSize - i - 1; j++) {
+                // Traverse the array from 0 to size of array[i-1] 
+                // Swap if the element found is greater than the next element
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void printArray(int[] input) {
+        System.out.println();
+        for (int i = 0; i < input.length; i++)
+            System.out.print(" " + input[i] + " ");
+        System.out.println();    
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {5,4,1,0,5,95,4,-100,200,0};
+        int arrSize = 10;
+        bubbleSort(arr, arrSize);
+        printArray(arr);
+    }
+}
---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,39 @@
+/**
+ * Runtime:
+ * Worst case O(n^2)
+ * Average case O(n^2)
+ * Best case O(n) - while loop will never run if list is already sorted, so O(n)
+ */
+class InsertionSort {
+  
+    static void insertionSort(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            int key = input[i];
+            int j = i - 1;
+            // Now compare the key (current idx) with rest, staring from left of current index till beginning
+            // If find elemetns larger than key, keep swapping until 
+            // either j reaches beginning or no more larger elems than key
+            while (j >= 0 && input[j] > key) {
+                if(input[j] > key){
+                    int tmp = input[j];
+                    input[j] = key;
+                    input[j + 1] = tmp;
+                    j--;
+                }
+            }
+        }
+    }
+  
+    static void printArray(int[] input) {
+        System.out.println();
+        for (int i = 0; i < input.length; i++)
+            System.out.print(" " + input[i] + " ");
+        System.out.println();    
+    }
+  
+    public static void main( String args[] ) {
+        int[] input = new int[] {7, 6, 5, 4, 3, 2, 1};
+        insertionSort(input);
+        printArray(input);
+    }
+}
